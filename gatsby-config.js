f50f582e39c0a65b450626394a4b266453a27ec2
JDavidYY <yonjosedavid@gmail.com>
2020-06-17 19:13:49 -0500

    Animaciones

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,23 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+           require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Montserrat:400,500,700`],
+        display: 'swap',
+      },
+    },
+   
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,23 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
 
-export default IndexPage
+
+const IndexPage = () => (
+  <div className="waveWrapper waveAnimation">
+  <div className="waveWrapperInner bgTop"> 
+    <div className="wave waveTop" style={{backgroundImage: `url(`+'http://front-end-noobs.com/jecko/img/wave-top.png'+`)`}}>
+    </div>
+    <h1 className="font-sans font-medium text-white text-center" >Hello. I 'm <span className="font-bold"> David Yon</span></h1>
+  </div>
+  
+  <div className="waveWrapperInner bgMiddle">
+    <div className="wave waveMiddle" style={{backgroundImage: `url(`+'http://front-end-noobs.com/jecko/img/wave-mid.png'+`)`}}></div>
+  </div>
+  <div className="waveWrapperInner bgBottom">
+    <div className="wave waveBottom" style={{backgroundImage:  `url(`+'http://front-end-noobs.com/jecko/img/wave-bot.png'+`)`}}></div>
+  </div>
+</div>
+);
+
+export default IndexPage;
